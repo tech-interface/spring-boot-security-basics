c4d527d053dce05c66691ff4d0ecbe50932e185a
tech-interface <sanketpatilcs@gmail.com>
2021-05-27 19:52:17 +0530

    updated files with name change

--- src/main/java/com/techinterface/SpringBootSecurityApplication.java
+++ src/main/java/com/techinterface/SpringBootSecurityApplication.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SwaggerIntegrationApplication {
-
+public class SpringBootSecurityApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(SwaggerIntegrationApplication.class, args);
+		SpringApplication.run(SpringBootSecurityApplication.class, args);
 	}
 
 }
